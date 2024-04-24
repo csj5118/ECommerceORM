ecf357e7b5a2e336da8d03169b8b243a6e0494a9
CJ <csh.joo77@gmail.com>
2024-04-23 21:29:40 -0400

    changed schema sql to make it work with the serial ID

--- db/schema.sql
+++ db/schema.sql
@@ -6,15 +6,25 @@ CREATE DATABASE ecommerce_db;
 
 
 
+
 CREATE TABLE Category (
     id SERIAL PRIMARY KEY,
-    category_name VARCHAR(50) NOT NULL
+    category_name VARCHAR(255) NOT NULL
 );
+
+
+INSERT INTO Category (category_name) VALUES
+('Shirts'),
+('Shorts'),
+('Music'),
+('Hats'),
+('Shoes');
+
 
 
 CREATE TABLE Product (
     id SERIAL PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     stock INT NOT NULL DEFAULT 10,
     category_id INT,
@@ -22,10 +32,31 @@ CREATE TABLE Product (
 );
 
 
+INSERT INTO Product (product_name, price, stock, category_id) VALUES
+('Plain T-Shirt', 14.99, 14, 1),
+('Running Sneakers', 90.00, 25, 5),
+('Branded Baseball Hat', 22.99, 12, 4),
+('Top 40 Music Compilation Vinyl Record', 12.99, 50, 3),
+('Cargo Shorts', 29.99, 22, 2);
+
+
+
 CREATE TABLE Tag (
     id SERIAL PRIMARY KEY,
-    tag_name VARCHAR(50)
+    tag_name VARCHAR(255)
 );
+
+
+INSERT INTO Tag (tag_name) VALUES
+('Rock Music'),
+('Pop Music'),
+('Blue'),
+('Red'),
+('Green'),
+('White'),
+('Gold'),
+('Pop Culture');
+
 
 
 CREATE TABLE ProductTag (
@@ -35,3 +66,21 @@ CREATE TABLE ProductTag (
     FOREIGN KEY (product_id) REFERENCES Product(id),
     FOREIGN KEY (tag_id) REFERENCES Tag(id)
 );
+
+
+INSERT INTO ProductTag (product_id, tag_id) VALUES
+(1, 6),
+(1, 7),
+(1, 8),
+(2, 6),
+(3, 1),
+(3, 3),
+(3, 4),
+(3, 5),
+(4, 1),
+(4, 2),
+(4, 8),
+(5, 3);
+
+
+select * from category c;
